b4c51dc6662708c7387967216b69a929b5d7659c
ofont <o.font@alumnos.upm.es>
2022-07-24 14:04:57 +0200

    lowdb local database successfully started

---
+++ adapters/database/Database.js
@@ -0,0 +1,42 @@
+import { Low, JSONFile } from 'lowdb'
+import { LogLevel } from '../../utils/LogLevel.js';
+
+/**
+ * @author Óscar Font
+ * ====================
+ * DatabaseAdapter class
+ * ====================
+ * @description
+ * Adapter Pattern applied to the database details.
+ * This class works as a wrapper of the database library.
+ * Currently uses LowDB, however if the library is wanted to be changed, 
+ * the only file needed to modify is this adapter class and the code would still work.
+ */
+
+class DatabaseAdapter {
+    #dbClient
+    #logger
+
+    constructor(logger) {
+        this.#logger = logger;
+        this.#dbClient = new Low(new JSONFile('./adapters/database/db.json'));
+        this.#logger.log(DatabaseAdapter.name, LogLevel.INFO, 'LowDB client successfully started');
+    }
+
+    getDBClient() {
+        return this.#dbClient;
+    }
+
+    async initDB() {
+        const data = {
+            user: [],
+            post: []
+        }
+        this.#dbClient.data = data;
+        await this.#dbClient.write();
+        this.#logger.log(DatabaseAdapter.name, LogLevel.INFO, 'User and Post collections successfully created');
+    }
+
+}
+
+export default DatabaseAdapter;
--- index.js
+++ index.js
@@ -1,11 +1,14 @@
+import DatabaseAdapter from './adapters/database/Database.js';
 import LoggerAdapter from './adapters/Logger.js';
 import ServerAdapter from './adapters/Server.js';
 import { LogLevel } from './utils/LogLevel.js';
 
-const Main = () => {
+const Main = async () => {
     const logger = new LoggerAdapter();
     const port = 3000;
     const server = new ServerAdapter(port, logger);
+    const db = new DatabaseAdapter(logger);
+    await db.initDB();
     server.start();
     logger.log(Main.name, LogLevel.INFO, 'Server sucessfully started at port ' + port);
 };
